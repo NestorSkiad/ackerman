0282b86f06d6f03d75700c5cf09c9eb22944c0f2
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-12-02 02:23:48 +0000

    I give up

--- ackerman.cpp
+++ ackerman.cpp
@@ -6,8 +6,24 @@ An efficient version of Ackerman's function.
  #include <stdlib.h>
  #include <string.h>
  #include <unordered_map>
+ #include <utility>
+ #include <functional>
+
+std::unordered_map<std::pair<int, int>, int> solution_dict;
+
+struct pair_hash{
+  template <class T1, class T2>
+
+  std::size_t operator () (const std::pair<T1, T2> &p) cont {
+    auto h1 = std::hash<T1> {}(p.first);
+    auto h2 = std::hash<T2> {}(p.first);
+
+
+  }
+}
 
  int main(int argc, char const *argv[]) {
-   
+   solution_dict[{2, 8}] = 5
+   printf("%s  %d\n", "hey", solution_dict[{2, 8}]);
    return 0;
  }
